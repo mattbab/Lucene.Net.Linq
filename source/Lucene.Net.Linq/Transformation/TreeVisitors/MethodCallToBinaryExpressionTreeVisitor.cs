797c0f0da873c8a362c780ac49cd31152a39aa34
m.babcock <matt.babcock@outlook.com>
2014-06-10 09:43:43 -0500

    Fixed similar to expression processing

--- source/Lucene.Net.Linq/Transformation/TreeVisitors/MethodCallToBinaryExpressionTreeVisitor.cs
+++ source/Lucene.Net.Linq/Transformation/TreeVisitors/MethodCallToBinaryExpressionTreeVisitor.cs
@@ -2,6 +2,7 @@
 using Lucene.Net.Linq.Search;
 using Lucene.Net.Search;
 using Remotion.Linq.Parsing;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace Lucene.Net.Linq.Transformation.TreeVisitors
@@ -14,26 +15,38 @@ namespace Lucene.Net.Linq.Transformation.TreeVisitors
     {
         protected override Expression VisitMethodCallExpression(MethodCallExpression expression)
         {
+            Expression[] args;
             var queryField = expression.Object as LuceneQueryFieldExpression;
 
-            if (queryField == null)
+            if (queryField != null)
+                args = expression.Arguments.ToArray();
+            else if (queryField == null && expression.Arguments.Count < 2)
                 return base.VisitMethodCallExpression(expression);
+            else
+            {
+                queryField = expression.Arguments[0] as LuceneQueryFieldExpression;
+
+                if (queryField != null)
+                    args = expression.Arguments.Skip(1).ToArray();
+                else
+                    return base.VisitMethodCallExpression(expression);
+            }
 
             if (expression.Method.Name == "StartsWith")
             {
-                return new LuceneQueryPredicateExpression(queryField, expression.Arguments[0], Occur.MUST, QueryType.Prefix);
+                return new LuceneQueryPredicateExpression(queryField, args[0], Occur.MUST, QueryType.Prefix);
             }
             if (expression.Method.Name == "EndsWith")
             {
-                return new LuceneQueryPredicateExpression(queryField, expression.Arguments[0], Occur.MUST, QueryType.Suffix);
+                return new LuceneQueryPredicateExpression(queryField, args[0], Occur.MUST, QueryType.Suffix);
             }
             if (expression.Method.Name == "Contains")
             {
-                return new LuceneQueryPredicateExpression(queryField, expression.Arguments[0], Occur.MUST, QueryType.Wildcard);
+                return new LuceneQueryPredicateExpression(queryField, args[0], Occur.MUST, QueryType.Wildcard);
             }
             if (expression.Method.Name == "SimilarTo")
             {
-                return new LuceneQueryPredicateExpression(queryField, expression.Arguments[0], Occur.MUST, QueryType.Fuzzy);
+                return new LuceneQueryPredicateExpression(queryField, args[0], Occur.MUST, QueryType.Fuzzy);
             }
 
             return base.VisitMethodCallExpression(expression);
